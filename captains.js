f9d3bed59953fc043281dce163787c4250d2231c
geleigh <57293957+geleigh@users.noreply.github.com>
2020-05-23 10:50:10 -0400

    updates
    updates

---
+++ captains.js
@@ -0,0 +1,5 @@
+    var url baseurl = "https://young-beach-08773.herokuapp.com/api/get_passengers"
+    function loadPassengers (){
+        var xmlhttp = new XMLHttpRequest()
+        xmlhttp.open("GET", baseurl)
+    }
