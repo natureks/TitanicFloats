93360e77bf7c3daa5212b259ae432355037d34a1
geleigh <57293957+geleigh@users.noreply.github.com>
2020-05-25 11:26:41 -0400

    delete
    delete

--- captains.js
+++
@@ -1,5 +0,0 @@
-    var url baseurl = "https://young-beach-08773.herokuapp.com/api/get_passengers"
-    function loadPassengers (){
-        var xmlhttp = new XMLHttpRequest()
-        xmlhttp.open("GET", baseurl)
-    }
