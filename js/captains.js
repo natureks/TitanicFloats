5ebdb7e10c4e4b80a5b495d2aa6a635da0053f8e
geleigh <57293957+geleigh@users.noreply.github.com>
2020-05-25 11:15:31 -0400

    fixed js
    fixed js

--- js/captains.js
+++ js/captains.js
@@ -6,24 +6,22 @@ var url = 'https://young-beach-08773.herokuapp.com/api/get_passengers'
 fetch(url)
     .then(res => res.json())//response type
     .then(data => addTable(data)); //log the data;
-
-function addTable (tableData) {
-  // remove all the existing table data if it exists
-  d3.selectAll('td').remove()
-
-  // add the rows
-  var tbody = d3.select('tbody')
-  tableData.forEach((sighting) => {
-    var row = tbody.append('tr')
-
-
-    var cell = row.append('td')
-    cell.text(sighting.age)
-  })
+    function addTable (tableData) {
+    // remove all the existing table data if it exists
+    d3.selectAll('td').remove()
+    // add the rows
+    var tbody = d3.select('tbody')
+    tableData.forEach((sighting) => {
+        var row = tbody.append('tr')
+        var cell;
+        // order the items in the dictionary to match with the html
+        orderArr = ['ticket', 'title', 'first_name', 'last_name', 'class', 'gender', 'sib_spouse', 'parents_child', 'fare', 'age', 'port', 'cabin', 'prob', 'survived'];
+        orderArr.forEach((col) => {
+            cell = row.append('td');
+            cell.text(sighting[col]);
+        })
+    })
 }
-
-// // initialize the table with all the data
-// addTable(data)
 
 // var url = 'https://young-beach-08773.herokuapp.com/api/get_passengers'
 
@@ -39,12 +37,9 @@ function addTable (tableData) {
 //   var tbody = d3.select('tbody')
 //   tableData.forEach((sighting) => {
 //     var row = tbody.append('tr')
-//     Object.entries(sighting.ticket) => {
+//     Object.entries(sighting).forEach(([key, value]) => {
 //       var cell = row.append('td')
 //       cell.text(value)
 //     })
 //   })
 // }
-
-// // // initialize the table with all the data
-// // addTable(data)
